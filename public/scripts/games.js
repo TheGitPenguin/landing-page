a703706e027b0ed99284b8b0faee4e4178ba1372
florian <la238872@student.helha.be>
2025-06-11 19:50:05 +0200

    add button action and game page

---
+++ public/scripts/games.js
@@ -0,0 +1,5 @@
+const tetrisCard = document.getElementById('tetris-card');
+
+tetrisCard.addEventListener('click', () => {
+    window.location.href = 'https://games.penggames.systems';
+});
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,13 @@
 const body = document.querySelector("body");
 const vpsCard = document.getElementById("vps-card");
+const gamesCard = document.getElementById("games-card");
 
 body.addEventListener('scroll', () =>
     {
         const scrollTop = window.scrollY;
         const scrollHeight = document.documentElement.scrollHeight;
         const clientHeight = document.documentElement.clientHeight;
-    
+
         if (scrollTop + clientHeight >= scrollHeight) {
         console.log("Reached the bottom of the page");
         }
@@ -16,5 +17,11 @@ body.addEventListener('scroll', () =>
 vpsCard.addEventListener('click', () =>
     {
         
+    }
+);
+
+gamesCard.addEventListener('click', () =>
+    {
+        window.location.href = "/games.html";
     }
 );
